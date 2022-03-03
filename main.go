b414f2e5a5f284478384d6d1263424b0a4a8d369
Henrique melo <henrique.melo@stone.com.br>
2022-03-03 13:40:46 -0300

    Debug

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"time"
 
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
