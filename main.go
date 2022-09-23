bd9f80f21aa81514a7ad221466bf1af64ba648d4
Matheus Marques Botelho <matheusmb1919@gmail.com>
2022-09-23 09:51:39 -0300

    feat(main): update dependencies

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"time"
 
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
 
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
-	certmanagerv1 "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
+	certmanagerv1 "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/nesv/go-dynect/dynect"
@@ -115,7 +116,7 @@ func (c *dynDNSProviderSolver) dynClient(cfg *dynDNSProviderConfig, namespace st
 		return nil, err
 	}
 
-	sec, err := c.client.CoreV1().Secrets(namespace).Get(cfg.PasswordSecretRef.LocalObjectReference.Name, metav1.GetOptions{})
+	sec, err := c.client.CoreV1().Secrets(namespace).Get(context.Background(), cfg.PasswordSecretRef.LocalObjectReference.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
